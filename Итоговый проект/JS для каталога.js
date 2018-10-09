63ef94c596bdc8b04230f921f1213b68db45fdad
Zhekakam <42640733+Zhekakam@users.noreply.github.com>
2018-10-09 19:26:14 +0200

    Add files via upload

---
+++ Итоговый проект/JS для каталога.js
@@ -0,0 +1,25 @@
+/*чтобы картинка выезжалана клик. Возникла проблема с массивами, тороплюсь.Ну нахер, сделаю кнопкой*/
+let sheet1 =document.querySelector('.sheet1');
+let sheet2 =document.querySelector('.sheet2');
+let sheet3 =document.querySelector('.sheet3');
+let sheet4 =document.querySelector('.sheet4');
+let sheet5 =document.querySelector('.sheet5');
+let sheet6 =document.querySelector('.sheet6');
+let sheet7 =document.querySelector('.sheet7');
+let sheet8 =document.querySelector('.sheet8');
+let sheet9 =document.querySelector('.sheet9');
+let sheet10 =document.querySelector('.sheet10');
+let sheet11 =document.querySelector('.sheet11');
+let sheet12 =document.querySelector('.sheet12');
+let sheet13 =document.querySelector('.sheet13');
+let sheet14 =document.querySelector('.sheet14');
+let sheet15 =document.querySelector('.sheet15');
+let sheet16 =document.querySelector('.sheet16');
+let sheet17 =document.querySelector('.sheet17');
+let sheet18 =document.querySelector('.sheet18');
+let sheet19 =document.querySelector('.sheet19');
+
+
+function myFunction(footer__photo){
+  alert ("Думал, чтобы красиво выезжало все изображение, но сделаю кнопкой")
+}
